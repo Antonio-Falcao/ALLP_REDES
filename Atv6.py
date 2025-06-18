16eb569efd72236e7c669963465d048173ce0870
Antonio-Falcao <antoniofalc@gmail.com>
2025-06-18 00:05:10 -0300

    Add files via upload

---
+++ Atv6.py
@@ -0,0 +1,46 @@
+'''def boas_vindas():
+    print(f"Olá, seja bem-vindo(a) a disciplina ALLP")
+boas_vindas()'''
+
+'''def quadrado(numero):
+    return numero ** 2
+
+resultado = quadrado()
+print(f"O quadrado deste número é:",(resultado))'''
+
+'''def somar(a,b):
+    return a + b
+   
+soma = somar(8, 9)
+print(f"A soma destes números é:", (soma))'''
+
+'''def contagem(inicio = 1, fim = 5):
+    for contagem in range(inicio, fim + 1):
+        print(contagem)
+contagem()'''
+
+'''def calcula_imc(peso = 70, altura = 1.75):
+    return peso / (altura ** 2)
+IMC = calcula_imc()
+print(f"O IMC é:", IMC)'''
+
+'''def par_ou_impar(numero):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Ímpar"
+resultado = par_ou_impar(8)
+print(resultado)
+
+resultado = par_ou_impar(9)
+print(resultado)'''
+
+'''def saudacao(nome = "Visitante"):
+    print(f"Seja bem-vindo(a)", {nome})
+saudacao()
+saudacao(nome = "João")'''
+
+
+        
+
+        
